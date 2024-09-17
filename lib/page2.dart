bae42c7d1d799e25175797dd2d3f08be1766f099
syabila123 <syabilamarhas@gmail.com>
2024-09-17 12:04:02 +0700

    latihan 4

--- lib/page2.dart
+++ lib/page2.dart
@@ -6,9 +6,38 @@ class Page2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('ini halaman 2'),
-      ),
+      body: Column(
+       children:[
+          Row(children: [
+            Container(
+           height: 100,
+            width: 100,
+            color: Colors.red,
+            ),
+            
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.green,
+              ),
+          ],
+          ),
+          Row(children: [
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.blue,
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.amber,
+            ),
+          ],
+          )
+        ],
+      )
     );
+  
   }
 }
